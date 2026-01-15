321ad7d77cf0230132280c6f5a759c835abcc984
Guzinh <guslessafim@gmail.com>
2026-01-15 19:40:45 -0300

    css

--- src/components/ListaDeTarefas/index.tsx
+++ src/components/ListaDeTarefas/index.tsx
@@ -5,6 +5,7 @@ import { NovaTarefa } from "../NovaTarefa";
 import Item from "../Item";
 import { useState } from "react";
 import { useContadorDeTarefas } from "@/hooks/useContadorDeTarefas";
+import style from "./ListaDeTarefas.module.css";
 
 export default function ListaDeTarefas({ data }: { data: Tarefa[] }) {
   const [tarefas, setTarefas] = useState(data);
@@ -17,12 +18,12 @@ export default function ListaDeTarefas({ data }: { data: Tarefa[] }) {
   };
 
   return (
-    <main>
-      <h1>Total de tarefas: {useContadorDeTarefas(tarefas)}</h1>
+    <main className={style.main}>
+      <h1 className={style.title}>Total de tarefas: {useContadorDeTarefas(tarefas)}</h1>
 
       <NovaTarefa onAdd={adicionarTarefa} />
 
-      <ul>
+      <ul className={style.lista}>
         {tarefas.map((t) => (
           <Item key={t.id} tarefa={t} onDelete={deletarTarefa} />
         ))}
--- src/components/NovaTarefa/index.tsx
+++ src/components/NovaTarefa/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import style from "./NovaTarefa.module.css";
 
 type Props = {
   onAdd: (tarefa: string) => void;
@@ -18,14 +19,15 @@ export function NovaTarefa({ onAdd }: Props) {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form className={style.form} onSubmit={handleSubmit}>
         <input
+          className={style.input}
           type="text"
           placeholder="Digite sua Tarefa"
           value={novaTarefa}
           onChange={(e) => setNovaTarefa(e.target.value)}
         />
-        <button type="submit">Adicionar Tarefa</button>
+        <button className={style.button} type="submit">Adicionar Tarefa</button>
       </form>
     </>
   );
